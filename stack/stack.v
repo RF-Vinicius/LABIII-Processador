9f629c7e9918570af48a88c9dd4ff891465e355d
RF-Vinicius <reisf.vinicius@gmail.com>
2023-12-13 22:12:29 -0300

    Ajuste ponteiro da pilha

--- stack/stack.v
+++ stack/stack.v
@@ -11,32 +11,45 @@ module stack
   output  empty   
 );
 
-
 reg [(WIDTH_DATA-1):0] stack[(DEPTH-1):0];
-reg [(DEPTH-1):0] topPositionStack;
+reg [(DEPTH-1):0] topPositionStack, topPositionStack_next;
 
 
 assign full = (topPositionStack == 10'b1111111111);
 assign empty = (topPositionStack == 10'b0000000000);
 
-
 always @(posedge clk) begin
     if (reset) begin
-
         topPositionStack <= 10'b0;
-
     end else begin
-
         if(push && !full) begin
             stack[topPositionStack] <= data_in;
-            topPositionStack <= topPositionStack + 1;
         end
         else if (pop && !empty) begin
-            topPositionStack <= topPositionStack - 1;
-            data_out <= stack[topPositionStack];
+            data_out <= stack[topPositionStack - 1];
         end
     end
 end
 
+
+// Adicionando lógica combinatória para calcular novo valor de topPositionStack
+always @* begin
+    if (push && !full) begin
+        topPositionStack_next = topPositionStack + 1;
+    end
+    else if (pop && !empty) begin
+        topPositionStack_next = topPositionStack - 1;
+    end
+    else begin
+        topPositionStack_next = topPositionStack;
+    end
+end
+
+// Atribuindo novo valor de topPositionStack
+always @(posedge clk) begin
+    if (!reset) begin
+        topPositionStack <= topPositionStack_next;
+    end
+end
 
 endmodule
